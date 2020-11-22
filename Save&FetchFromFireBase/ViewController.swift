213b9c6ad4fd09a51912f1eebab70e13913483de
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-11-22 11:59:45 +0200

    Initial Commit

---
+++ Save&FetchFromFireBase/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Save&FetchFromFireBase
+//
+//  Created by Volodymyr Ostapyshyn on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var quoteLabel: UILabel!
+    @IBOutlet var quoteTextField: UITextField!
+    @IBOutlet var authorTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveTapped(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
+
