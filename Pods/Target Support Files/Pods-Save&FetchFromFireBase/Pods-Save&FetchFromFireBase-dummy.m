b85f87420cad6e1d9c1e3647295a91ebf08a1ea5
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-11-22 12:30:44 +0200

    Save to FireStore

---
+++ Pods/Target Support Files/Pods-Save&FetchFromFireBase/Pods-Save&FetchFromFireBase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Save_FetchFromFireBase : NSObject
+@end
+@implementation PodsDummy_Pods_Save_FetchFromFireBase
+@end
--- Save&FetchFromFireBase/ViewController.swift
+++ Save&FetchFromFireBase/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -13,13 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet var quoteTextField: UITextField!
     @IBOutlet var authorTextField: UITextField!
     
+    var docRef: DocumentReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        docRef = Firestore.firestore().document("sampleData/Inspiration")
     }
     
     @IBAction func saveTapped(_ sender: UIButton) {
-        
+        guard let quoteText = quoteTextField.text, !quoteText.isEmpty else { return }
+        guard let quoteAuthor = authorTextField.text, !quoteAuthor.isEmpty else { return }
+        let dataToSave: [String: Any] = ["quote": quoteText, "author": quoteAuthor]
+        docRef.setData(dataToSave) { error in
+            if let error = error {
+                print("Error: ", error.localizedDescription)
+            } else {
+                print("Data has been saved!")
+            }
+        }
     }
     
 
